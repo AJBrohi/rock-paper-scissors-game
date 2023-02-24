a4657176dd45c44354a6bdd0a5e999b7fe1291e1
AJBrohi <brohi96@gmail.com>
2023-02-25 02:11:43 +0600

    code done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock Paper Scissors Game
+
+A Python-based game of rock-paper-scissors where players can compete against the computer. Make your choice and see if you can win against computer!
---
+++ main.py
@@ -0,0 +1,78 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user_played = int(input('What do you want to play? Type 0 for "Rock", 1 for "Paper" or 2 for "Scissiors"\n'))
+
+computer_played = random.randint(0,2)
+
+print("You played:")
+if user_played == 0:    
+    print("Rock\n"+rock)
+    
+    print("Computer played:")
+    if computer_played == 0:
+        print("Rock\n"+rock)        
+        print("DRAW!")
+    elif computer_played == 1:
+        print("Paper\n"+paper)        
+        print("YOU LOSE!")
+    else:
+        print("Scissors\n"+scissors)        
+        print("YOU WIN!")
+        
+elif user_played == 1:    
+    print("Paper\n"+paper)
+    
+    print("Computer played:")
+    if computer_played == 0:
+        print("Rock\n"+rock)        
+        print("YOU WIN!")
+    elif computer_played == 1:
+        print("Paper\n"+paper)        
+        print("DRAW!")
+    else:
+        print("Scissors\n"+scissors)        
+        print("YOU LOSE!")
+        
+elif user_played == 2:    
+    print("Scissors\n"+scissors)
+    
+    print("Computer played:")
+    if computer_played == 0:
+        print("Rock\n"+rock)        
+        print("YOU LOSE!")
+    elif computer_played == 1:
+        print("Paper\n"+paper)        
+        print("YOU WIN!")
+    else:
+        print("Scissors\n"+scissors)        
+        print("DRAW!")
+        
+else:
+    print("Wrong Selection. Try Again.")
